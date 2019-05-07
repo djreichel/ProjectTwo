036a2b68520f4f49603b6202cf399ef5c0314850
picasso <46543347+djreichel@users.noreply.github.com>
2019-05-06 20:23:51 -0400

    adding data to schema

--- models/schema.sql
+++ models/schema.sql
@@ -1,5 +1,13 @@
-DROP DATABASE IF EXISTS exampledb;
-CREATE DATABASE exampledb;
+DROP DATABASE IF EXISTS beerReviewsdb;
+CREATE DATABASE beerReviewsdb;
+USE beerReviewsdb;
 
-DROP DATABASE IF EXISTS testdb;
-CREATE DATABASE testdb;
+CREATE TABLE beers(
+  id INTEGER(11) AUTO_INCREMENT NOT NULL,
+  author_name VARCHAR(255) NOT NULL,
+  beer_name VARCHAR(150) NOT NULL,
+  beer_type VARCHAR(50),
+  beer_rating INTEGER(11),
+  beer_review VARCHAR(500) NOT NULL,
+  PRIMARY KEY (id)
+);
