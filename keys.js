a0f03c0fd415fac760b63917a95758c2af84e366
meowster93 <cmendoza6277@yahoo.com>
2019-05-11 11:52:32 -0400

    kind of working

---
+++ keys.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-console */
+/* eslint-disable no-undef */
+
+exports.api = {
+    id: process.env.SPOTIFY_ID,
+    secret: process.env.SPOTIFY_SECRET
+  };  
+  
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -3,19 +3,21 @@ var db = require("../models");
 module.exports = function(app) {
   // Load index page
   app.get("/", function(req, res) {
-    db.Example.findAll({}).then(function(dbExamples) {
-      res.render("index", {
-        msg: "Welcome!",
-        examples: dbExamples
-      });
+    let fakeData = ["beerName", "Author"];
+    // db.beerReviews.findAll({}).then(function(beerReviewsdb) {
+     
+    // });
+    res.render("index", {
+      msg: "Welcome!",
+      beerReviews: fakeData
     });
   });
 
-  // Load example page and pass in an example by id
-  app.get("/example/:id", function(req, res) {
-    db.Example.findOne({ where: { id: req.params.id } }).then(function(dbExample) {
-      res.render("example", {
-        example: dbExample
+  // Load review page and pass in a review by id
+  app.get("/beerReviews/:id", function(req, res) {
+    db.beerReviews.findOne({ where: { id: req.params.id } }).then(function(beerReviewsdb) {
+      res.render("beerReviews", {
+        beerReviews: beerReviewsdb
       });
     });
   });
